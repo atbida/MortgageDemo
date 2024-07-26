2fbbc8417ce07528e57efe33630fd62f10e07bdf
bobw@prophecy.io <bobw@prophecy.io>
2024-07-26 12:54:30 +0000

    Update prophecy-libs dependencies to version 3.3.0-8.0.29 across jobs and pipelines

--- jobs/job_MortgageAnalytics_AF/code/_76fqaamguzx5dgo_w0fykw_/tasks/mortgage_analytics.py
+++ jobs/job_MortgageAnalytics_AF/code/_76fqaamguzx5dgo_w0fykw_/tasks/mortgage_analytics.py
@@ -45,7 +45,7 @@ def mortgage_analytics(ti=None, params=None, **context):
             "entry_point": "main", 
             "parameters": ["-i", "default", "-O", "{}"]
           }, 
-          "libraries": [{"maven" : {"coordinates" : "io.prophecy:prophecy-libs_2.12:3.3.0-8.0.25"}},                          {"pypi" : {"package" : "prophecy-libs==1.9.9"}},                          {
+          "libraries": [{"maven" : {"coordinates" : "io.prophecy:prophecy-libs_2.12:3.3.0-8.0.29"}},                          {"pypi" : {"package" : "prophecy-libs==1.9.9"}},                          {
                            "whl": "dbfs:/FileStore/prophecy/artifacts/saas/app/__PROJECT_ID_PLACEHOLDER__/__PROJECT_RELEASE_VERSION_PLACEHOLDER__/pipeline/mortgage_analytics-1.0-py3-none-any.whl"
                          }]
         },
